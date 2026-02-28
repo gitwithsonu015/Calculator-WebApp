36e7a795ca12e71d7ae8d1227e630da60e62abaf
gitwithsonu015 <sonudigital245@gmail.com>
2026-02-28 23:13:32 +0530

    Add basic calculator functionality in script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+let display = document.getElementById("display");
+let history = document.getElementById("history");
+
+function append(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    let exp = display.value;
+    let result = eval(exp);
+    display.value = result;
+
+    let li = document.createElement("li");
+    li.textContent = exp + " = " + result;
+    history.appendChild(li);
+  } catch {
+    display.value = "Error";
+  }
+}
+
+function toggleDark() {
+  document.body.classList.toggle("dark");
+}
